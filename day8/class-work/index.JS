2c345c85fc6fe3e68cf0011c4b3aa12576f6e803
FUYGJHG <sudrasanmal@gmail.com>
2025-02-20 13:02:15 +0530

    thggyftrty

---
+++ day8/class-work/index.JS
@@ -0,0 +1,24 @@
+// document.getElementById("parent").style.border="1px solid gray";
+// document.getElementById("parent").style.height="50px";
+// document.getElementById("parent").style.backgroundColor="blue"
+// document.getElementById("parent").style.border="1px solid gray";
+// let element=document.createElement("button")
+
+// element.innerText="submit";
+// element.style.backgroundColor="red"
+// element.style.padding="10px";
+// document.getElementById("parent").append(element)
+// newFunction();
+
+
+    // console.log(variable);
+    // variable = 15;
+    console.log(variable);
+
+    let variable = 10;
+function newFunction() {
+    
+   
+    console.log(variable);
+}
+newFunction()
