03c2ed5dfc0d0b739f25b35673a88c93e6073d0b
FUYGJHG <sudrasanmal@gmail.com>
2025-02-24 20:04:35 +0530

    fh

---
+++ day10/class-work/practice.js
@@ -0,0 +1,114 @@
+let str="SV University"
+console.log(str.length)
+str[0]
+console.log(str[0])
+console.log(str.length-1)
+
+let arr=[1,2,3,"sp",1.2,true,false,null]
+console.log(arr[3])
+
+console.log(arr.length-1)
+arr.push("push")
+console.log(arr)
+arr[4]="ranit"
+arr.pop()
+console.log(arr)
+//object
+// object Literal
+//new key word
+//constructer
+//let svuClass= ["sudrasan","b.te",3]
+
+const obj={
+    sudarsan:{class:"b.tech",year:3,subject:["DBMS","nm","fs"]}
+
+}
+obj["1"]="praven"//for replace
+delete obj["1"]
+console.log(obj.sudarsan)//another method of acess
+console.log(obj["sudarsan"])//acess the value of sudarsan string
+// console.log(obj.sudarsan.subject[0])
+obj.virat={
+    calss:"vssi"
+}
+
+console.log(obj)
+objbj.virat={
+    calss:"cci"
+}
+console.log(obj)
+let Class="BCii"
+let name="preet"
+let sub="FS"
+let newObj=[name,class,sub]
+console.log(newObj)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'
---
+++ day9/class-work/day9.js
@@ -0,0 +1,35 @@
+//+,-,*,/,%,**
+//=,+=,-`=,*=,**=,!=
+// let a=20
+// a*=10
+// console.log(a);
+//==,===,<,,>,>=,<=,!,!=,!==,
+// let str= "  "
+// console.log(str.length)
+// let a="19", b="10"
+// let c= -a+-b
+// console.log(typeof(c))
+// console.log(a+b)
+// console.log(typeof(a))
+// console.log(typeof(b))
+// if(a===b){
+//     console.log("a is eqaul to b")
+// }
+// else{
+//     console.log(" a is not equal to b")
+// }
+let a=10
+let b=20;
+myFuntyion(a,b)
+
+function myFuntyion(f,g){
+    console.log(f+g)
+
+
+}
+const myFuntyion=(f,g)=>{
+    console.log(f+g)
+}
+(()=>{
+    console.log("IIFE")
+})()
